9021fcc55373211168dd37c15a090ce05e86c53d
Juan Manuel Alonso <juanma@nubesys.io>
2020-03-03 19:55:47 -0300

    Se conecta el datadb con phalcon4

--- injections/db.php
+++ injections/db.php
@@ -4,14 +4,31 @@
 
 $globalDI->set('datadb', function() use ($globalDI) {
 
-    $db = new Phalcon\Db\Adapter\Pdo\Mysql((array)$globalDI->get('config')->connections->mydata);
+    $connectionName         = $globalDI->get('config')->main->datadb->connection;
 
-    return $db;
+    if(!is_null($globalDI->get('config')->connections->$connectionName)){
+
+        $connectionData     = $globalDI->get('config')->connections->$connectionName;
+        
+        $dbOptions       = [
+            'host'          => $connectionData->host,
+            'username'      => $connectionData->user,
+            'password'      => $connectionData->pass,
+            'dbname'        => $connectionData->name,                    
+        ];
+        
+        return new Phalcon\Db\Adapter\Pdo\Mysql($dbOptions);
+    }else{
+        
+        return false;
+    } 
 
 },true);
 
 $globalDI->set('utildb', function() use ($globalDI) {
 
     $db = new Phalcon\Db\Adapter\Pdo\Mysql((array)$globalDI->get('config')->connections->myutil);
+
+    return $db;
 
 },true);
